595bdd021de30b85128b3f0d8bef354c529607b4
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2024-01-08 19:40:58 +0530

    Add files via upload

---
+++ Day7_AdditionOfTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AdditionOfTwoNumbers
+{
+     public static void main(String[]args)
+{
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the value ");
+     int X =sc.nextInt();
+     int Y =sc.nextInt();
+     int sum = X + Y;
+      System.out.println(sum);
+}
+}
