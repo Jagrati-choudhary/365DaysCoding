a1831fb563235ed2be694ddb89c69332d921df2f
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2024-01-09 12:44:05 +0530

    Add files via upload

---
+++ Day9_FindTransitionPoint.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class FindTransitionPoint
+{
+        public static void main(String args[])
+{
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Emter the size of array: ");
+        int n = sc.nextInt();
+	int[] a = new int[n];
+       
+	       for (int i = 0; i < n; i++)
+            {
+	       System.out.print("a[" + i + "]=");
+	       a[i] = sc.nextInt();
+              
+             
+            } 
+              for(int i=0;i<n;i++){
+                 if(a[i]==1){
+                System.out.println(i);
+                    break;
+           
+            }
+        }
+        System.out.println("-1");
+}
+}
