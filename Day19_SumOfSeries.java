e7198787df9a52b70121d145236523a79092812c
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2024-01-19 12:53:32 +0530

    Add files via upload

---
+++ Day19_SumOfSeries.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SumOfSeries
+{
+            public static void main(String[] args)
+    {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enetr a number");
+            int n = sc.nextInt();
+            System.out.println (n*(n+1)/2);
+     }
+}
