aebd4d131fc76cb64fb046cce4c200d878f3ce47
Jagrati-choudhary <146639935+Jagrati-choudhary@users.noreply.github.com>
2024-02-03 18:47:47 +0530

    Add files via upload

---
+++ Day34_TheXORGate.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class TheXORGate
+{
+       public static void main(String args[])
+   {
+
+          Scanner sc = new Scanner(System.in);
+		System.out.print("Emter the Size: ");
+		int N = sc.nextInt();
+         int arr[]=new int [N];
+         for (int i=0;i<N;i++){
+          System.out.println("arr["+i+"]=" );
+            arr[i] = sc.nextInt();
+       }
+          int a = 0;
+        for(int i=0;i<N;i++){
+            a =  a ^ arr[i]; 
+        }
+        System.out.println( a);
+    }
+}
